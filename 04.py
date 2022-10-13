b807c14d8868d725aa0cba03c5d89358c6aa053b
Maxim <maxiprav88@gmail.com>
2022-10-13 19:57:48 +0300

    sss

---
+++ 04.py
@@ -0,0 +1,9 @@
+# A (3,6); B (2,1) -> 5,09
+# A (7,-5); B (1,-1) -> 7,21
+
+print('Enter coordinates point А:')
+x_A = float(input('X: '))
+y_A = float(input('Y: '))
+print("Enter coordinates point B:")
+x_B = float(input('X: '))
+y_B = float(input('Y: '))
